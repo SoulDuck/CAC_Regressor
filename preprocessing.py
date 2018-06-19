b174d599cf05ba752f7bd3ecee0fec2c3bd8e2f2
seongjungkim <plznw4me@naver.com>
2018-06-19 16:44:18 +0900

    crawling date and make train , test ,validation data

--- preprocessing.py
+++ preprocessing.py
@@ -109,4 +109,6 @@ if '__main__' == __name__:
         train_cacs.extend([cac_score]*len(tmp_paths))
     print len(train_paths)
     print len(train_cacs)
+    print train_cacs[:10]
+    print train_cacs[-10:]
 
