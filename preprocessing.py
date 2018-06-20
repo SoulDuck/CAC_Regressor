0d6bd29b697dc95fe52b4cc7c4c4437002312619
seongjungkim <plznw4me@naver.com>
2018-06-20 18:02:09 +0900

    crawling date and make train , test ,validation data

--- preprocessing.py
+++ preprocessing.py
@@ -293,10 +293,24 @@ def make_data(data_id):
         lab_1_train_paths, lab_1_train_cacs = extract_paths_cacs(lab_1_train[:], img_dir)
         lab_0_train_paths , lab_0_train_cacs = extract_paths_cacs(lab_0_train[:], img_dir)
 
+        lab_1_val_paths, lab_1_val_cacs = extract_paths_cacs(lab_1_val[:], img_dir)
+        lab_0_val_paths , lab_0_val_cacs = extract_paths_cacs(lab_0_val[:], img_dir)
+
+        lab_1_test_paths, lab_1_test_cacs = extract_paths_cacs(lab_1_test[:], img_dir)
+        lab_0_test_paths , lab_0_test_cacs = extract_paths_cacs(lab_0_test[:], img_dir)
 
         imgs_0 = paths2numpy(lab_0_train_paths, None)
         imgs_1 = paths2numpy(lab_1_train_paths, None)
-        make_tfrecord(train_tfrecord_path, None, (len(imgs_0), imgs_0) , (len(imgs_1), imgs_1) , (len(imgs_0), imgs_0) ,(len(imgs_0), imgs_0))
+        make_tfrecord(train_tfrecord_path, None, (len(imgs_0), imgs_0) , (len(imgs_0), imgs_1))
+
+        imgs_0 = paths2numpy(lab_0_val_paths, None)
+        imgs_1 = paths2numpy(lab_1_val_paths, None)
+        make_tfrecord(train_tfrecord_path, None, (len(imgs_0), imgs_0) , (len(imgs_1), imgs_1) )
+
+        imgs_0 = paths2numpy(lab_0_test_paths, None)
+        imgs_1 = paths2numpy(lab_1_test_paths, None)
+        make_tfrecord(train_tfrecord_path, None, (len(imgs_0), imgs_0) , (len(imgs_1), imgs_1))
+
 
 if '__main__' == __name__:
     make_data(data_id='0100-0000003-019')
