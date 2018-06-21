1c5dd3aa144ed600ffd378ecec29ef5cd6a3089c
seongjungkim <plznw4me@naver.com>
2018-06-22 01:34:58 +0900

    crawling date and make train , test ,validation data

--- preprocessing.py
+++ preprocessing.py
@@ -443,9 +443,9 @@ def make_data(data_id , img_dir ='/home/mediwhale/fundus_harddisk/merged_reg_fun
         lab_0_train, lab_0_val, lab_0_test = divide_paths_TVT(lab_0, 75, 75)
         lab_1_train, lab_1_val, lab_1_test = divide_paths_TVT(lab_1, 75, 75)
 
-        train_tfrecord_path = './train_0_10_11_inf.tfrecord'
-        test_tfrecord_path = './test_0_10_11_inf.tfrecord'
-        val_tfrecord_path = './val_0_10_11_inf.tfrecord'
+        train_tfrecord_path = './0100-0000003-022/train_0_10_11_inf.tfrecord'
+        test_tfrecord_path = './0100-0000003-022/test_0_10_11_inf.tfrecord'
+        val_tfrecord_path = './0100-0000003-022/val_0_10_11_inf.tfrecord'
 
         lab_1_train_paths, lab_1_train_cacs = extract_paths_cacs(lab_1_train[:], img_dir)
         lab_0_train_paths , lab_0_train_cacs = extract_paths_cacs(lab_0_train[:], img_dir)
