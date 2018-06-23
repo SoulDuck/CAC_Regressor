cb6e556769fd4732dcdfbaf36ec2295c85a7c68d
kimseongjung <plznw4me@naver.com>
2018-06-23 15:08:49 +0900

    for test

--- preprocessing.py
+++ preprocessing.py
@@ -361,15 +361,21 @@ def make_data(data_id , img_dir ='/home/mediwhale/fundus_harddisk/merged_reg_fun
 
         lab_1_val_paths, lab_1_val_cacs = extract_paths_cacs(lab_1_val[:], img_dir)
         lab_0_val_paths , lab_0_val_cacs = extract_paths_cacs(lab_0_val[:], img_dir)
-        """
-        print len(lab_0_val_paths + lab_1_val_paths)
-        for i,path in enumerate(lab_1_val_paths + lab_0_val_paths):
+
+        f = open('./val_labels.txt' 'w')
+        for i,path in enumerate(lab_1_val_paths):
             name=os.path.splitext(os.path.split(path)[-1])[0]
             img = np.asarray(Image.open(path))
             plt.imsave('tmp/{}.png'.format(name) , img)
+            f.write(str(1)+'\n')
 
-        exit()
-        """
+
+        for i,path in enumerate(lab_0_val_paths):
+            name=os.path.splitext(os.path.split(path)[-1])[0]
+            img = np.asarray(Image.open(path))
+            plt.imsave('tmp/{}.png'.format(name) , img)
+            f.write(str(0)+'\n')
+
 
         lab_1_train_paths, lab_1_train_cacs = extract_paths_cacs(lab_1_train[:], img_dir)
         lab_0_train_paths , lab_0_train_cacs = extract_paths_cacs(lab_0_train[:], img_dir)
