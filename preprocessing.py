c2543001e4fb6c6395c0a52330338c540b89930e
kimseongjung <plznw4me@naver.com>
2018-06-23 13:45:13 +0900

    for test

--- preprocessing.py
+++ preprocessing.py
@@ -361,8 +361,8 @@ def make_data(data_id , img_dir ='/home/mediwhale/fundus_harddisk/merged_reg_fun
 
         lab_1_test_paths, lab_1_test_cacs = extract_paths_cacs(lab_1_test[:], img_dir)
         lab_0_test_paths , lab_0_test_cacs = extract_paths_cacs(lab_0_test[:], img_dir)
-        print len(lab_1_test_paths)
-        for path in lab_1_test_paths + lab_0_test_paths:
+        print len(lab_0_val_paths +lab_1_val_paths  )
+        for path in lab_1_val_paths + lab_0_val_paths:
             print path
         exit()
 
